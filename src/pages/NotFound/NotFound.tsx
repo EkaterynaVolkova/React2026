a9a28450112c78ef1f934e7a9442b8ecc19ade18
Ekaterina Volkova <volkova5005@gmail.com>
2026-05-16 11:44:47 +0300

    feat: 404 Page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,14 @@
 import { Link } from 'react-router';
+import './NotFound.css';
 
 export const NotFound = () => {
   return (
-    <div>
-      This page doesn&apos;t exist. Go <Link to="/">Home</Link>
+    <div className="error-page__container">
+      <h1 className="code">404</h1>
+      <h2 className="title">OOPS! This page doesn&apos;t exist</h2>
+      <Link className="button home-link" to="/">
+        Go Home
+      </Link>
     </div>
   );
 };

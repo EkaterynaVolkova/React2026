31db0c8cd4329a91eadb00a118e7a235cc5e89ae
Ekaterina Volkova <volkova5005@gmail.com>
2026-05-28 21:07:31 +0300

    chore: add react query dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,34 @@ import { About } from './pages/About';
 import { Layout } from '@components/Layout';
 import { CharacterDetails } from '@components/CharacterDetails';
 import { ThemeProvider } from './context/ThemeProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <>
-      <ThemeProvider>
-        <ErrorBoundary>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route path="/" element={<Search />}>
-                <Route path="" element={<CharacterDetails />} />
-              </Route>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <ErrorBoundary>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route path="/" element={<Search />}>
+                  <Route path="" element={<CharacterDetails />} />
+                </Route>
 
-              <Route path="about" element={<About />} />
-              <Route path="*" element={<NotFound />} />
-            </Route>
-          </Routes>
-        </ErrorBoundary>
-      </ThemeProvider>
+                <Route path="about" element={<About />} />
+                <Route path="*" element={<NotFound />} />
+              </Route>
+            </Routes>
+          </ErrorBoundary>
+        </ThemeProvider>
+        <ReactQueryDevtools
+          initialIsOpen={false}
+          buttonPosition="bottom-left"
+        />
+      </QueryClientProvider>
     </>
   );
 };

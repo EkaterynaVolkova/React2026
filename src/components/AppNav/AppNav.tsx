559d08e99fec5325ca888cfb6d45cc0c8b152d52
Ekaterina Volkova <volkova5005@gmail.com>
2026-06-22 09:50:56 +0300

    feat: redo links

--- src/components/AppNav/AppNav.tsx
+++ src/components/AppNav/AppNav.tsx
@@ -1,5 +1,5 @@
 import { getTranslations } from 'next-intl/server';
-import Link from 'next/link';
+import { Link } from '@/i18n/navigation';
 import { ThemeSwitcher } from '@/components/ThemeSwitcher';
 import { LangSwitcher } from '../LangSwitcher';
 
